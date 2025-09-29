bea661be80b9520a088bda7195ae655a5854f033
yashwanth4621v <yashwanthboui666@gmail.com>
2025-09-29 09:04:15 +0530

    Update DBUtil.java

--- src/utils/DBUtil.java
+++ src/utils/DBUtil.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBUtil {
     private static final String URL = "jdbc:mysql://localhost:3306/hotel_booking";
     private static final String USER = "root"; // change if needed
-    private static final String PASSWORD = "COD@4621v"; // put your MySQL password
+    private static final String PASSWORD = "password"; // put your MySQL password
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
